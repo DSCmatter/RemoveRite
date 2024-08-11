80b909bb3cb8d56a0bb24602190d159a059aa69f
DSCmatter <136594818+DSCmatter@users.noreply.github.com>
2024-08-11 16:38:34 +0530

    modified app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os 
 from flask import Flask, render_template, request, send_file
 from rembg import remove
 from PIL import Image
@@ -24,4 +25,5 @@ def upload_file():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5100)
+    port = int(os.environ.get('PORT', 5100))  
+    app.run(host='0.0.0.0', port=port, debug=True)
